1abe18d1504f2d4fdd9157788f7acfef80cb0bd1
embbnux <embbnux@embbnux.com>
2019-06-24 13:29:15 +0800

    Updates

---
+++ precache-manifest.03845255f9bd341aaab861d7e3e4e72c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47205ed4e3827e09e7ca6a75f5831c24",
+    "url": "/ringcentral-presences-monitor/index.html"
+  },
+  {
+    "revision": "860faeb28c284909a212",
+    "url": "/ringcentral-presences-monitor/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "db8f2f4152a773e8ddc5",
+    "url": "/ringcentral-presences-monitor/static/js/2.a3d07a2a.chunk.js"
+  },
+  {
+    "revision": "860faeb28c284909a212",
+    "url": "/ringcentral-presences-monitor/static/js/main.4d468102.chunk.js"
+  },
+  {
+    "revision": "5eb0b61a64144895fd56",
+    "url": "/ringcentral-presences-monitor/static/js/runtime~main.72a09043.js"
+  }
+]);
